fb99c4d9974558b80cb43c7e46e102644f6bf388
lizhe <southgate@qq.com>
2012-09-24 22:57:59 +0800

    2

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,12 @@
+var app = app || {};
+
+
+      var contextPath = "http://localhost:8080/pm";
+      brite.defaultComponentConfig.loadTmpl = true;
+
+
+(function($){
+	brite.display("MainScreen",null,{parent:"#bodyPage"});
+
+})();
+
